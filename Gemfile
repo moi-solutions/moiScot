1ebfc3fa79af4d6e0e17eea525dfa426cb205b93
José Ney Guerrero <joseneyguerrero@gmail.com>
2014-12-18 17:34:08 -0600

    added gems and initial setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+gem "lita"
+gem "lita-slack"
+gem "lita-google-images"
+gem "lita-karma"
+gem 'lita-chuck_norris'
+gem "lita-logger"
+gem "lita-whois"
+
+gem 'dotenv'
+gem "puma"
---
+++ lita_config.rb
@@ -0,0 +1,35 @@
+Dotenv.load
+
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "moiScot"
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  # config.robot.adapter = :shell
+  # config.adapters.shell.private_chat
+  ## Example: Set options for the chosen adapter.
+  config.robot.adapter = :slack
+  config.adapters.slack.token = ENV['SLACK_BOT_KEY']
+
+  ## Example: Set options for the Redis connection.
+  # config.redis.host = "127.0.0.1"
+  # config.redis.port = 1234
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  config.handlers.google_images.safe_search = :off
+end
